4a92daef4547715a16624d459c2f13f290853048
Alejandro D'Ambrosio <alejandro.dambrosio@gmail.com>
2016-04-11 11:34:38 -0300

    Se sacó la opción --watch para levantar las apps porque reiniciaba el server

--- startApps.sh
+++ startApps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Starting apps..."
 rm log/alumno.log log/docente.log log/server.log
-pm2 start apps.json --watch
+pm2 start apps.json
 sleep 2
 multitail -s 3 -i log/server.log -i log/alumno.log -i log/docente.log
